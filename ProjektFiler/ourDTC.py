1d759ed641e42533f01f23339d32bfcb8b321dc5
augustalex <augustalexandersson@gmail.com>
2017-02-09 15:59:29 +0100

    Begun implementing Gini

--- ProjektFiler/ourDTC.py
+++ ProjektFiler/ourDTC.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 import pandas
 import sklearn.model_selection
 
-from specialNode import Node
+from specialNode import Node, split
 import numpy as np
 
 
@@ -25,30 +25,60 @@ def new_mean_value_test(subjects, featureIndex):
 
     return test
 
+def new_gini_test(subjects, feature_index):
+    #children = split(subject, )
+
+    class_frequency = defaultdict(int)
+
+    for subject in subjects:
+        class_frequency[subject.class_label] += 1
+
+    n_classes = len(subjects)
+    gini_index = 1
+    for class_key in class_frequency.keys():
+        gini_index -= (class_frequency[class_key] / n_classes) ** 2
+
+
+def child_gini_index(child, subjects):
+
+    
+
+
+def start_hunts(data_features, data_class_labels):
+    subjects = [Subject(row, label) for row, label in zip(data_features, data_class_labels)]
+
+    model = Node()
+    hunts(model, subjects, 0)
+
+    return model
+
 
 def hunts(parent_node, subjects, feature_index):
     # Base-Case: If there are no features left, then return.
     if feature_index == len(subjects[0].features):
-        print("Most common: " + most_common_class_label(subjects))
         parent_node.label = most_common_class_label(subjects)
-
-    if group_has_same_label(subjects):
+    elif group_has_same_label(subjects):
         parent_node.label = subjects[0].class_label
     else:
         """Constructs a test with the mean value of subjects
-                current feature (feature_index), so we know where to split."""
+        current feature (feature_index), so we know where to split."""
         test = new_mean_value_test(subjects, feature_index)
+        parent_node.split_test = test
 
         # Splits the node with the new feature test
-        for split_node in parent_node.split(subjects, test):
-            parent_node.child_nodes.append(split_node.node)
-            hunts(split_node.node, split_node.subjects, feature_index + 1)
+        split_nodes = split(subjects, test)
+        if len(split_nodes) <= 1:
+            hunts(parent_node, subjects, feature_index + 1)
+        else:
+            for split_node in split_nodes:
+                parent_node.child_nodes.append(split_node.node)
+                hunts(split_node.node, split_node.subjects, feature_index + 1)
 
 
 def most_common_class_label(subjects):
     result_set = defaultdict(int)
     for subject in subjects:
-        result_set[subject.class_label] += 1
+        result_set[subject.class_label[0]] += 1
 
     return max(result_set, key=result_set.get)
 
@@ -64,6 +94,7 @@ def group_has_same_label(subjects):
     # They all have the same class label
     return True
 
+
 df = pandas.read_csv(r"C:\Users\August\Documents\ILS Projekt\ILS Projekt Dataset\csv_binary\binary\labor.csv",
                      header=None)
 
@@ -78,28 +109,42 @@ X_arr.astype(np.double)
 # Bosco Talkshow
 # Training set, test set, train klass label, test klass label. We split
 # into sets
-X, X_test, y, y_test = sklearn.model_selection\
+X, X_test, y, y_test = sklearn.model_selection \
     .train_test_split(X_arr, y_arr, test_size=0.33, random_state=42)
 
-X.astype(np.double)
-
 X = [[float(n) for n in row] for row in X]
+X_test = [[float(n) for n in row] for row in X_test]
 
 
 def test_hunts():
-    subjects = [Subject(row, label) for row, label in zip(X, y)]
+    model = start_hunts(X, y)
 
-    node = Node()
-    hunts(node, subjects, 0)
-    print_node(node)
+    y_test_predict = predict(model, X_test)
+    compare_results(y_test_predict, y_test)
 
 
-def print_node(node):
+def compare_results(prediction, correct_result):
+    correct = 0
+    for i in range(len(prediction)):
+        if prediction[i] == correct_result[i]:
+            correct += 1
+
+    percentage = correct / len(prediction)
+    print("Correct to " + str(percentage*100) + "%")
+
+
+def predict(node, test_subjects):
+    subjects = [Subject(row, None) for row in test_subjects]
+    return [get_class_for_subject(node, subject) for subject in subjects]
+
+
+def get_class_for_subject(node, subject):
     if not node.label:
-        print(node.test)
+        return get_class_for_subject(
+            node.get_child_from_test(subject), subject
+        )
     else:
-        print("class: " + node.label)
-    for child in node.child_nodes:
-        print_node(child)
+        return node.label
+
 
 test_hunts()
--- specialNode.py
+++ specialNode.py
@@ -1,24 +1,51 @@
 from collections import defaultdict, namedtuple
 
-SplitNode = namedtuple('SplitNode', ['node', 'subjects'])
+SplitNodeSubjects = namedtuple('SplitNode', ['node', 'subjects'])
+
+''' We create the node class to hold the label, test: for when we use our model for the
+    test data. Ofcourse we also need a reference to the child nodes.'''
+
 
 class Node:
-
-    def __init__(self):
+    def __init__(self, split_value=None):
+        self.split_value = split_value
+        self.split_test = None
         self.label = None
-        self.test = None
         self.child_nodes = []
-
-    def split(self, subjects, test):
-        self.test = test
-        splits = defaultdict(list)
-        for subject in subjects:
-            result_option = test(subject)
-            splits[result_option].append(subject)
-
-        return [SplitNode(node=Node(self), subjects=splits[key]) for key in splits.keys()]
 
     def set_class_label(self, label):
         self.label = label
 
+    ''' Here we get the right child node from the test, if
+        we don't have a test or else: we throw a exception. '''
 
+    def get_child_from_test(self, subject):
+        if self.split_test is None:
+            raise Exception('Node has no split test.')
+
+        result = self.split_test(subject)
+        for child in self.child_nodes:
+            if child.split_value == result:
+                return child
+
+        raise Exception('No suitable path for subject in Node.')
+
+
+def split(subjects, split_test):
+    """ here we take care of the splitting of the
+        nodes and group them correctly"""
+
+    # Creates a dictionary where all empty keys are lists
+    splits = defaultdict(list)
+    for subject in subjects:
+        test_result = split_test(subject)
+        # Groups the subject with the result of the split_test
+        splits[test_result].append(subject)
+
+    # Puts the group of subjects into new child nodes
+    return [
+        SplitNodeSubjects(
+            node=Node(test_result),
+            subjects=splits[test_result])
+        for test_result in splits.keys()
+        ]
