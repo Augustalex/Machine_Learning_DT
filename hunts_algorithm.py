70597f5b279e87f60f060e55aa554fc8ff94102d
augustalex <augustalexandersson@gmail.com>
2017-03-14 13:11:50 +0100

    Added formatted test data

--- hunts_algorithm.py
+++ hunts_algorithm.py
@@ -52,7 +52,7 @@ def hunts(parent_node, subjects, depth, min_samples_leaf=10, max_depth=100):
         current feature (feature_index), so we know where to split."""
 
         # Uses gini to generate the best split out of all features.
-        best_gini_split = generate_best_split_of_all_features(subjects, Entropy)
+        best_gini_split = generate_best_split_of_all_features(subjects, Gini)
 
         # Stores the test of the "best split" in the parent node, for future prediction with that node.
         parent_node.split_test = best_gini_split.test
