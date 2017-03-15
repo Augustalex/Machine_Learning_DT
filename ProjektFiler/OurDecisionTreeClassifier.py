9d2e53b03f3219b9e0e01ead5070f09063fcb4ff
optimusprime <s143522@student.hb.se>
2017-03-15 15:58:03 +0100

    added comments and fixed a bugg in random forrest fit

--- ProjektFiler/OurDecisionTreeClassifier.py
+++ ProjektFiler/OurDecisionTreeClassifier.py
@@ -49,24 +49,35 @@ class OurDecisionTreeClassifier:
 class OurRandomForrestClassifier:
 
     def __init__(self, sample_size, max_features, criterion=Gini, max_depth=None, min_sample_leaf=1, bagging=True, n_estimators=10):
+        # criterion: gini or entropy
         self.criterion = criterion
         self.max_features = max_features
         self.max_depth = max_depth
         self.min_sample_leaf = min_sample_leaf
         self.bagging = bagging
-        self.sample_size = sample_size #value between 0-1
+        self.sample_size = sample_size
         self.n_estimators = n_estimators
 
         self.estimators = None
 
+    """
+        Our random-forest fit function recieves training data and if bagging is true: loops through
+        all the estimators and inserts random samples until (sample size * total rows) is
+        reached. When the sample data for a estimator is filled we create a decision tree
+        classifier and fit the tree to that data and add it to our estimators list for predictions
+        in the next step.
+    """
     def fit(self, features_train, class_labels_train):
-        samples_x = []
-        samples_y = []
+        # total number of rows
         n_rows = len(class_labels_train)
+        # total sample size to use: by multiplying with total rows.
         size = n_rows * self.sample_size
         if not self.bagging == False:
             for dt in range(self.n_estimators):
+                samples_x = []
+                samples_y = []
                 for i in range(size):
+                    # in each loop we append an random row of data to our samples list.
                     index = numpy.random.randint(0, n_rows)
                     samples_x.append(features_train[index])
                     samples_y.append(class_labels_train[index])
